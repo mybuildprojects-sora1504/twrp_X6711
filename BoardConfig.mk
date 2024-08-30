ab2eb241949d452feadc7b1532244a0897749906
akifakif32 <mhmtkfdrn@gmail.com>
2024-08-30 19:07:08 +0300

    changing name

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/infinix/Infinix-X6711
+DEVICE_PATH := device/infinix/X6711
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
@@ -41,7 +41,7 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 OVERRIDE_TARGET_FLATTEN_APEX := true
 
 # Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := Infinix-X6711
+TARGET_BOOTLOADER_BOARD_NAME := X6711
 TARGET_NO_BOOTLOADER := true
 
 # Display
@@ -59,8 +59,8 @@ BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-TARGET_KERNEL_CONFIG := Infinix-X6711_defconfig
-TARGET_KERNEL_SOURCE := kernel/infinix/Infinix-X6711
+TARGET_KERNEL_CONFIG := X6711_defconfig
+TARGET_KERNEL_SOURCE := kernel/infinix/X6711
 
 # Kernel - prebuilt
 TARGET_FORCE_PREBUILT_KERNEL := true
